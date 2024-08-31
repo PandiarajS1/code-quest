00745edbe6e56b4ce7153e351e00bf4c12c1636d
PandiarajS1 <s.pandiarajsekar@gmail.com>
2024-08-31 21:44:46 +0530

    fifth commit

--- client/src/Comnponent/map/map.js
+++ client/src/Comnponent/map/map.js
@@ -5,8 +5,8 @@ import axios from 'axios'
 import './map.css'
 
 const Map = () => {
-
   const [weather,setWeather] = useState(null)
+  const [location,setlocation] = useState(null)
   let apiKey = 'ba2fc15b6296413f6b8aab62ffe3cc49'
 
     useEffect(()=>{
@@ -18,7 +18,6 @@ const Map = () => {
         attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }
     ).addTo(map)
-
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(position => {
         const latitude = position.coords.latitude;
@@ -26,7 +25,7 @@ const Map = () => {
         map.setView([latitude,longitude],13)
 
         fetchweather(latitude,longitude)
-
+        getlocation(latitude,longitude)
         const marker = L.marker([latitude,longitude]).addTo(map)
         marker.bindPopup("you're here").openPopup();
       },(error)=>{
@@ -41,11 +40,19 @@ const Map = () => {
 
         const fetchweather = async(lat,lon)=>{
           try {
-            console.log(typeof lat,lon)
             const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=ba2fc15b6296413f6b8aab62ffe3cc49`)
             setWeather(res.data)
           } catch (error) {
             console.log(error.response.data)
+          }
+        }
+
+        const getlocation = async(lat,lon)=> {
+          try {
+            const location = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}&zoom=10&addressdetails=1`)
+            setlocation(location.data.address)
+          } catch (error) {
+            console.log(error)
           }
         }
 
@@ -61,6 +68,16 @@ const Map = () => {
         </div>
       )
     }
+      {
+      location && (
+        <div className='weather-container'>
+          <p>City :{location.city}</p>
+          <p>State: {location.state}</p>
+          <p>Country : {location.country}</p>
+        </div>
+      )
+      }
+    
     </div>
   )
 }
